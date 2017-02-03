6a3f0c58fac949b2e6f56022c5d77a72be3a143b
Niclas Snellman <nici@bambuser.com>
2017-02-03 10:49:50 +0100

    Correct indentation

--- src/jsmpeg.js
+++ src/jsmpeg.js
@@ -15,75 +15,75 @@ var JSMpeg = {
 	},
 };
 
-	// The Player sets up the connections between source, demuxer, decoders,
-	// renderer and audio output. It ties everything together, is responsible
-	// of scheduling decoding and provides some convenience methods for
-	// external users.
-	require('./player')(JSMpeg);
+// The Player sets up the connections between source, demuxer, decoders,
+// renderer and audio output. It ties everything together, is responsible
+// of scheduling decoding and provides some convenience methods for
+// external users.
+require('./player')(JSMpeg);
 
-	// A Video Element wraps the Player, shows HTML controls to start/pause
-	// the video and handles Audio unlocking on iOS. VideoElements can be
-	// created directly in HTML using the <div class="jsmpeg"/> tag.
-	require('./video-element')(JSMpeg);
+// A Video Element wraps the Player, shows HTML controls to start/pause
+// the video and handles Audio unlocking on iOS. VideoElements can be
+// created directly in HTML using the <div class="jsmpeg"/> tag.
+require('./video-element')(JSMpeg);
 
-	// The BitBuffer wraps a Uint8Array and allows reading an arbitrary number
-	// of bits at a time. On writing, the BitBuffer either expands its
-	// internal buffer (for static files) or deletes old data (for streaming).
-	require('./buffer')(JSMpeg);
+// The BitBuffer wraps a Uint8Array and allows reading an arbitrary number
+// of bits at a time. On writing, the BitBuffer either expands its
+// internal buffer (for static files) or deletes old data (for streaming).
+require('./buffer')(JSMpeg);
 
-	// A Source provides raw data from HTTP, a WebSocket connection or any
-	// other mean. Sources must support the following API:
-	//   .connect(destinationNode)
-	//   .write(buffer)
-	//   .start() - start reading
-	//   .resume(headroom) - continue reading; headroom to play pos in seconds
-	//   .established - boolean, true after connection is established
-	//   .completed - boolean, true if the source is completely loaded
-	//   .progress - float 0-1
-	require('./ajax')(JSMpeg);
-	require('./ajax-progressive')(JSMpeg);
-	require('./websocket')(JSMpeg);
+// A Source provides raw data from HTTP, a WebSocket connection or any
+// other mean. Sources must support the following API:
+//   .connect(destinationNode)
+//   .write(buffer)
+//   .start() - start reading
+//   .resume(headroom) - continue reading; headroom to play pos in seconds
+//   .established - boolean, true after connection is established
+//   .completed - boolean, true if the source is completely loaded
+//   .progress - float 0-1
+require('./ajax')(JSMpeg);
+require('./ajax-progressive')(JSMpeg);
+require('./websocket')(JSMpeg);
 
-	// A Demuxer may sit between a Source and a Decoder. It separates the
-	// incoming raw data into Video, Audio and other Streams. API:
-	//   .connect(streamId, destinationNode)
-	//   .write(buffer)
-	//   .currentTime – float, in seconds
-	//   .startTime - float, in seconds
-	require('./ts')(JSMpeg);
-	require('./player')(JSMpeg);
+// A Demuxer may sit between a Source and a Decoder. It separates the
+// incoming raw data into Video, Audio and other Streams. API:
+//   .connect(streamId, destinationNode)
+//   .write(buffer)
+//   .currentTime – float, in seconds
+//   .startTime - float, in seconds
+require('./ts')(JSMpeg);
+require('./player')(JSMpeg);
 
-	// A Decoder accepts an incoming Stream of raw Audio or Video data, buffers
-	// it and upon `.decode()` decodes a single frame of data. Video decoders
-	// call `destinationNode.render(Y, Cr, CB)` with the decoded pixel data;
-	// Audio decoders call `destinationNode.play(left, right)` with the decoded
-	// PCM data. API:
-	//   .connect(destinationNode)
-	//   .write(pts, buffer)
-	//   .decode()
-	//   .seek(time)
-	//   .currentTime - float, in seconds
-	//   .startTime - float, in seconds
-	require('./decoder')(JSMpeg);
-	require('./mpeg1')(JSMpeg);
-	require('./mp2')(JSMpeg);
+// A Decoder accepts an incoming Stream of raw Audio or Video data, buffers
+// it and upon `.decode()` decodes a single frame of data. Video decoders
+// call `destinationNode.render(Y, Cr, CB)` with the decoded pixel data;
+// Audio decoders call `destinationNode.play(left, right)` with the decoded
+// PCM data. API:
+//   .connect(destinationNode)
+//   .write(pts, buffer)
+//   .decode()
+//   .seek(time)
+//   .currentTime - float, in seconds
+//   .startTime - float, in seconds
+require('./decoder')(JSMpeg);
+require('./mpeg1')(JSMpeg);
+require('./mp2')(JSMpeg);
 
-	// A Renderer accepts raw YCrCb data in 3 separate buffers via the render()
-	// method. Renderers typically convert the data into the RGBA color space
-	// and draw it on a Canvas, but other output - such as writing PNGs - would
-	// be conceivable. API:
-	//   .render(y, cr, cb) - pixel data as Uint8Arrays
-	//   .enabled - wether the renderer does anything upon receiving data
-	require('./canvas2d')(JSMpeg);
-	require('./webgl')(JSMpeg);
+// A Renderer accepts raw YCrCb data in 3 separate buffers via the render()
+// method. Renderers typically convert the data into the RGBA color space
+// and draw it on a Canvas, but other output - such as writing PNGs - would
+// be conceivable. API:
+//   .render(y, cr, cb) - pixel data as Uint8Arrays
+//   .enabled - wether the renderer does anything upon receiving data
+require('./canvas2d')(JSMpeg);
+require('./webgl')(JSMpeg);
 
-	// Audio Outputs accept raw Stero PCM data in 2 separate buffers via the
-	// play() method. Outputs typically play the audio on the user's device.
-	// API:
-	//   .play(sampleRate, left, right) - rate in herz; PCM data as Uint8Arrays
-	//   .stop()
-	//   .enqueuedTime - float, in seconds
-	//   .enabled - wether the output does anything upon receiving data
-	require('./webaudio')(JSMpeg);
+// Audio Outputs accept raw Stero PCM data in 2 separate buffers via the
+// play() method. Outputs typically play the audio on the user's device.
+// API:
+//   .play(sampleRate, left, right) - rate in herz; PCM data as Uint8Arrays
+//   .stop()
+//   .enqueuedTime - float, in seconds
+//   .enabled - wether the output does anything upon receiving data
+require('./webaudio')(JSMpeg);
 
 module.exports = JSMpeg;
